2ee725a44878caad53a798fd106dc8d991e6e1cd
langmead <langmead>
2008-07-08 14:25:23 +0000

    *** empty log message ***

---
+++ tests/paper_eval/kg_competition/driver.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+dir=`pwd`
+NAME=`basename $dir | sed 's/_.*//'`
+echo Using NAME: ${NAME}
+
+sh ebwt.sh 2>&1 | tee ${NAME}.ebwt.out
+sh maq.sh 2>&1  | tee ${NAME}.maq.out
+sh soap.sh 2>&1 | tee ${NAME}.soap.out
