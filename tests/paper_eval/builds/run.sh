5e23dea122dc41318c4596ef42610f89d1b4da91
langmead <langmead>
2008-07-08 21:17:08 +0000

    Small bugfix - truncate .top file before appending

--- tests/paper_eval/builds/run.sh
+++ tests/paper_eval/builds/run.sh
@@ -6,12 +6,15 @@ echo Using NAME: ${NAME}
 
 ./ebwt_build --version
 
+echo > ${NAME}.ebwt_build.esa.top
 sh wrap.sh ${NAME}.ebwt_build.esa \
     ./ebwt_build -d --entireSA -v hs_ref_${NAME}.mfa ${NAME}.esa \
     2>&1 | tee ${NAME}.ebwt_build.esa.out
+echo > ${NAME}.ebwt_build.blf.top
 sh wrap.sh ${NAME}.ebwt_build.blf \
     ./ebwt_build -d --bmax 3000000000 -v hs_ref_${NAME}.mfa ${NAME}.blf \
     2>&1 | tee ${NAME}.ebwt_build.blf.out
+echo > ${NAME}.ebwt_build.bl7.top
 sh wrap.sh ${NAME}.ebwt_build.bl7 \
     ./ebwt_build -d --bmaxDivN 7 -v hs_ref_${NAME}.mfa ${NAME}.bl7 \
     2>&1 | tee ${NAME}.ebwt_build.bl7.out
