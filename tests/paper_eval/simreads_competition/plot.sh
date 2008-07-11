d1a9f98d894b4c699a7d458efc814f464bfc354b
langmead <langmead>
2008-07-11 19:49:14 +0000

    *** empty log message ***

--- tests/paper_eval/simreads_competition/plot.sh
+++ tests/paper_eval/simreads_competition/plot.sh
@@ -7,5 +7,8 @@ while [ $# -ge 1 ]; do
 	cd ../$1
 	sh summarize_all_top.sh > $dir/$name.results.txt
 	cd $dir
+	awk '{print $2}' $name.results.txt | cut -d, -f 1 > $name.results.times.txt
+	awk '{print $2}' $name.results.txt | cut -d, -f 2 > $name.results.vmmax.txt
+	awk '{print $2}' $name.results.txt | cut -d, -f 3 > $name.results.rsmax.txt
 	shift
 done
