bc8216186baa715b5151032bbfdf72d6c1dc79e8
langmead <langmead>
2008-07-11 19:44:48 +0000

    *** empty log message ***

--- tests/paper_eval/simreads_competition/plot.sh
+++ tests/paper_eval/simreads_competition/plot.sh
@@ -3,8 +3,9 @@
 dir=`pwd`
 while [ $# -ge 1 ]; do
 	name=`$1 | sed 's/_.*//'`
-	echo Doing dataset $name in directory ../$1
+	echo "Doing dataset $name in directory ../$1"
 	cd ../$1
 	sh summarize_all_top.sh > $dir/$name.results.txt
 	cd $dir
+	shift
 done
