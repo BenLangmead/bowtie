dbdcdd5cf16e754c953b0217ae23eeefdec05432
langmead <langmead>
2008-07-11 20:30:49 +0000

    *** empty log message ***

--- tests/paper_eval/simreads_competition/plot.pl
+++ tests/paper_eval/simreads_competition/plot.pl
@@ -7,6 +7,7 @@ defined($ARGV[0]) || die "Must specify run names";
 my @runnames = @ARGV; # -> column names
 
 open(RUNTIME, ">runtime.tex") || die "Could not open >runtime.tex";
+print RUNTIME "\\begin{document}\n";
 print RUNTIME "\\begin{tabular}{ | l || ";
 for(my $i = 0; $i <= $#runnames; $i++) {
 	print RUNTIME "r | ";
@@ -17,6 +18,7 @@ print RUNTIME " & Human Chromosome 22 & Human Chromosome 2 & Whole Human Genome 
 print RUNTIME "\\end{tabular}\n";
 
 open(MEMORY, ">memory.tex") || die "Could not open >memory.tex";
+print MEMORY "\\begin{document}\n";
 print MEMORY "\\begin{tabular}{ | l || ";
 for(my $i = 0; $i <= $#runnames; $i++) {
 	print MEMORY "r | ";
