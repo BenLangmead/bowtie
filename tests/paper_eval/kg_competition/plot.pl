af929aef07068ecae3c8bb272d57ea68e35d77b9
langmead <langmead>
2008-07-16 15:05:09 +0000

    *** empty log message ***

---
+++ tests/paper_eval/kg_competition/plot.pl
@@ -0,0 +1,5 @@
+#!/usr/bin/perl -w
+
+use strict;
+use warnings;
+
--- tests/paper_eval/kg_competition/plot.sh
+++ tests/paper_eval/kg_competition/plot.sh
@@ -19,12 +19,15 @@
 dir=`pwd`
 NAME=`basename $dir | sed 's/_.*//'`
 echo Using NAME: ${NAME}
+WORKSTATION=1
 if [ ! -f $NAME.results.txt ] ; then
 	sh summarize_all_top.sh > $NAME.results.txt
 fi
 if [ ! -f $NAME.maps.txt ] ; then
 	sh analyze_maps.sh > $NAME.maps.txt
 fi
+
+# For a workstation:
 
 # Bowtie
 echo -n "Bowtie," > $NAME.results.bowtie.txt
@@ -43,5 +46,19 @@ echo -n "Maq," > $NAME.results.maq.txt
 tmp=`head -3 $NAME.results.txt | tail -1 | cut -d" " -f 2 | cut -d, -f 1`
 echo -n "$tmp," >> $NAME.results.maq.txt
 head -3 $NAME.maps.txt | tail -1 | cut -d":" -f 3 >> $NAME.results.maq.txt
+
+if [ "$WORKSTATION" = "0" ] ; then
+	# SOAP -v 1
+	echo -n "SOAP -v 1," > $NAME.results.soap.v1.txt
+	tmp=`head -4 $NAME.results.txt | tail -1 | cut -d" " -f 2 | cut -d, -f 1`
+	echo -n "$tmp," >> $NAME.results.soap.v1.txt
+	head -6 $NAME.maps.txt | tail -1 | cut -d":" -f 3 >> $NAME.results.soap.v1.txt
+
+	# SOAP -v
+	echo -n "SOAP," > $NAME.results.soap.txt
+	tmp=`head -5 $NAME.results.txt | tail -1 | cut -d" " -f 2 | cut -d, -f 1`
+	echo -n "$tmp," >> $NAME.results.soap.txt
+	head -5 $NAME.maps.txt | tail -1 | cut -d":" -f 3 >> $NAME.results.soap.txt
+fi
 
 perl plot.pl
