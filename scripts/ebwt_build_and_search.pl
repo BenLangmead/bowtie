8c08cc07ba952abb5609711e409461774dba1e61
langmead <langmead>
2008-07-23 22:26:47 +0000

    *** empty log message ***

--- scripts/ebwt_build_and_search.pl
+++ scripts/ebwt_build_and_search.pl
@@ -11,11 +11,16 @@ my $verbose = $options{v} if defined($options{v});
 defined($ARGV[0]) || die "Must define texts as first argument";
 defined($ARGV[1]) || die "Must define patterns as second argument";
 
+my $text = shift @ARGV;
+my $pats = shift @ARGV;
+my $args = "";
+$args = join(" ", @ARGV) if defined(@ARGV);
+
 my $cmd;
-$cmd = "./ebwt_build-with-asserts  -c -d -s $ARGV[0] .tmp2";
+$cmd = "./ebwt_build-with-asserts -c -d -s $text .tmp2";
 print "$cmd\n" if $verbose;
 system($cmd);
 
-$cmd = "./ebwt_search-with-asserts -c -1 --concise -a -s --orig $ARGV[0] .tmp2 $ARGV[1]";
+$cmd = "./ebwt_search-with-asserts $args -c -1 --concise -a -s --orig $text .tmp2 $pats";
 print "$cmd\n" if $verbose;
 system($cmd);
