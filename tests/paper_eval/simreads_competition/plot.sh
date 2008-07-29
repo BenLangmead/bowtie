e03f3cf7a3df1e888b87c0a87158f979dac5994f
langmead <langmead>
2008-07-29 14:38:06 +0000

    *** empty log message ***

--- tests/paper_eval/simreads_competition/plot.sh
+++ tests/paper_eval/simreads_competition/plot.sh
@@ -21,15 +21,17 @@ while [ $# -ge 1 ]; do
 	echo "Doing dataset $name in directory ../$1"
 	cd ../$1
 	# Omit all1 results, since we do not expect those to be relevant
-	sh summarize_all_top.sh | grep -v all1 > $dir/$name.results.txt
-	cd $dir
-	awk '{print $1}' $name.results.txt > $name.results.names.txt
+	if [ ! -f $dir/$name.results.txt ] ; then
+		sh summarize_all_top.sh | grep -v all1 | grep -v n1 | grep -v v1 > $dir/$name.results.txt
+	fi
+	#cd $dir
+	#awk '{print $1}' $name.results.txt > $name.results.names.txt
 	# Wall clock time
-	awk '{print $2}' $name.results.txt | cut -d, -f 2 > $name.results.times.txt
+	#awk '{print $2}' $name.results.txt | cut -d, -f 2 > $name.results.times.txt
 	# Max VM footprint
-	awk '{print $2}' $name.results.txt | cut -d, -f 3 > $name.results.vmmax.txt
+	#awk '{print $2}' $name.results.txt | cut -d, -f 3 > $name.results.vmmax.txt
 	# Max RES footprint
-	awk '{print $2}' $name.results.txt | cut -d, -f 4 > $name.results.rsmax.txt
+	#awk '{print $2}' $name.results.txt | cut -d, -f 4 > $name.results.rsmax.txt
 	shift
 done
 
