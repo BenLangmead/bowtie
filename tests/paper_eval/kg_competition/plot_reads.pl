c06bf9c2cd018fa6d742dc0ec0377e8b0f3be3c9
langmead <langmead>
2008-07-29 18:56:29 +0000

    *** empty log message ***

--- tests/paper_eval/kg_competition/plot_reads.pl
+++ tests/paper_eval/kg_competition/plot_reads.pl
@@ -1,5 +1,12 @@
 #!/usr/bin/perl -w
 
+sub commaize {
+	my $s = shift;
+	return $s if length($s) <= 3;
+	my $t = commaize(substr($s, 0, length($s)-3)) . "," . substr($s, length($s)-3, 3);
+	return $t;
+}
+
 defined($ARGV[0])
 	|| die "Must specify number of reads mapped by both Maq and Bowtie as first argument";
 defined($ARGV[1])
@@ -12,11 +19,47 @@ my $inebwt = int($ARGV[1]);
 my $inmaq  = int($ARGV[2]);
 my $tot = $inboth + $inebwt + $inmaq;
 
-my $bothpct = sprintf("%2.1f%%", $inboth * 100.0 / $tot);
-my $ebwtpct = sprintf("%2.1f%%", $inebwt * 100.0 / $tot);
-my $maqpct  = sprintf("%2.1f%%", $inmaq  * 100.0 / $tot);
+my $inbothc = commaize($inboth);
+my $inebwtc = commaize($inebwt);
+my $inmaqc  = commaize($inmaq);
+my $totc    = commaize($tot);
+
+my $bothpct = sprintf("%2.1f", $inboth * 100.0 / $tot);
+my $ebwtpct = sprintf("%2.1f", $inebwt * 100.0 / $tot);
+my $maqpct  = sprintf("%2.1f", $inmaq  * 100.0 / $tot);
+
+#
+# Print plain-text summary
+#
 
 print "     In either: $tot\n";
-print "       In both: $inboth ($bothpct)\n";
-print "In Bowtie only: $inebwt ($ebwtpct)\n";
-print "   In Maq only: $inmaq ($maqpct)\n";
+print "       In both: $inboth ($bothpct%)\n";
+print "In Bowtie only: $inebwt ($ebwtpct%)\n";
+print "   In Maq only: $inmaq ($maqpct%)\n";
+
+#
+# Print TeX
+#
+
+system("cp headerinc.tex reads.tex") == 0 || die ("Must have headerinc.tex");
+open(READS, ">>reads.tex") || die "Could not open >>reads.tex";
+print READS "\\begin{document}\n";
+print READS "\\begin{table}[tp]\n";
+print READS "\\scriptsize\n";
+print READS "\\begin{tabular}{cccc}\n";
+
+print READS "\\multicolumn{4}{c}{Reads mapped} \\\\[3pt] \n";
+print READS "By either Maq & By both Maq & By Maq but & By Bowtie   \\\\ \n";
+print READS "or Bowtie     & and Bowtie  & not Bowtie & but not Maq \\\\ \n";
+print READS "\\toprule\n";
+print READS "$totc & $inbothc ($bothpct\\%) & $inebwtc ($ebwtpct\\%) & $inmaqc ($maqpct\\%) \\\\ \n";
+print READS "\\bottomrule\n";
+
+print READS "\\end{tabular}\n";
+print READS "\\caption{".
+	"Blah".
+	"}\n";
+print READS "\\end{table}\n";
+print READS "\\end{document}\n";
+
+close(READS);
