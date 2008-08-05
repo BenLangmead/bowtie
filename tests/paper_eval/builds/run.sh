7ccd06edae5181df9e7322c9568309d59788c3eb
langmead <langmead>
2008-08-05 04:36:42 +0000

    *** empty log message ***

--- tests/paper_eval/builds/run.sh
+++ tests/paper_eval/builds/run.sh
@@ -17,27 +17,27 @@ if [ ! -f ${NAME}.blf.1.ebwt ] ; then
 	    2>&1 | tee ${NAME}.ebwt_build.blf.out
 fi
 # Probably the fastest configuration that fits in a 8GB memory budget
-# ./ebwt_build --d --bmaxDivN 7
+# ./ebwt_build --d --bmaxdivn 7
 if [ ! -f ${NAME}.bl4.1.ebwt ] ; then
 	echo > ${NAME}.ebwt_build.bl4.top
 	sh wrap.sh ${NAME}.ebwt_build.bl4 \
-	    ./ebwt_build -d --bmaxDivN 4 -v hs_ref_${NAME}.mfa ${NAME}.bl4 \
+	    ./ebwt_build -d --bmaxdivn 4 -v hs_ref_${NAME}.mfa ${NAME}.bl4 \
 	    2>&1 | tee ${NAME}.ebwt_build.bl4.out
 fi
-# ./ebwt_build_packed -d --bmaxDivN 25 --dcv 4096
+# ./ebwt_build_packed -d --bmaxdivn 25 --dcv 4096
 if [ "$DO_SMALL_FOOTPRINT" = "1" ] ; then
 	# Probably the fastest configuration that fits in a 4GB memory budget
 	if [ ! -f ${NAME}.pkl.1.ebwt ] ; then
 		echo > ${NAME}.ebwt_build.pkl.top
 		sh wrap.sh ${NAME}.ebwt_build.pkl \
-		    ./ebwt_build -d --bmaxDivN 25 --dcv 4096 -v hs_ref_${NAME}.mfa ${NAME}.pkl \
+		    ./ebwt_build -d --bmaxdivn 25 --dcv 4096 -v hs_ref_${NAME}.mfa ${NAME}.pkl \
 		    2>&1 | tee ${NAME}.ebwt_build.pkl.out
 	fi
 	# Probably the fastest configuration that fits in a 2GB memory budget
 	if [ ! -f ${NAME}.pkt.1.ebwt ] ; then
 		echo > ${NAME}.ebwt_build.pkt.top
 		sh wrap.sh ${NAME}.ebwt_build.pkt \
-		    ./ebwt_build_packed -d --bmaxDivN 25 --dcv 4096 -v hs_ref_${NAME}.mfa ${NAME}.pkt \
+		    ./ebwt_build_packed -d --bmaxdivn 25 --dcv 4096 -v hs_ref_${NAME}.mfa ${NAME}.pkt \
 		    2>&1 | tee ${NAME}.ebwt_build.pkt.out
 	fi
 fi
