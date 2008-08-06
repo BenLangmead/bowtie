67d95e719a1fe89602540ef27fed3a0ad8ecaa72
langmead <langmead>
2008-08-06 21:30:12 +0000

    *** empty log message ***

--- tests/paper_eval/kg_competition/ebwt.sh
+++ tests/paper_eval/kg_competition/ebwt.sh
@@ -9,12 +9,6 @@ EXTRA_EXT=
 DO_CVS_UPDATE=0
 USE_FILTERED_READS=0
 
-# Possibly switch to filtered read set
-if [ "$USE_FILTERED_READS" = "1" ] ; then
-	READS=kg_reads_filt.fq
-	echo "Using filtered reads; READ_BASE is $READ_BASE"
-fi
-
 # Optionally do a cvs update in the Bowtie home
 if [ "$DO_CVS_UPDATE" = "1" ] ; then
 	pushd ${BOWTIE_HOME}
@@ -33,13 +27,28 @@ fi
 cp ${BOWTIE_HOME}/ebwt_search .
 ./ebwt_search --version
 
-if [ ! -f ${READS} ] ; then
-	echo "Could not find reads file ${READS}!  Aborting..."
+if [ ! -f ${NAME}.1.ebwt ] ; then
+	echo "Could not ebwt index file ${NAME}.1.ebwt!  Aborting..."
 	exit 1
 fi
 
-if [ ! -f ${NAME}.1.ebwt ] ; then
-	echo "Could not ebwt index file ${NAME}.1.ebwt!  Aborting..."
+
+begin:
+
+
+# Possibly switch to filtered read set
+if [ "$USE_FILTERED_READS" = "1" ] ; then
+	READS=kg_reads_filt.fq
+	EXTRA_EXT=_filt
+	echo "Using filtered reads; READ_BASE is $READ_BASE"
+else
+	READS=kg_reads.fq
+	EXTRA_EXT=
+	echo "Using unfiltered reads; READ_BASE is $READ_BASE"
+fi
+
+if [ ! -f ${READS} ] ; then
+	echo "Could not find reads file ${READS}!  Aborting..."
 	exit 1
 fi
 
@@ -59,4 +68,28 @@ if [ ! -f ${NAME}.ebwt.n1.${EXTRA_EXT}hits ] ; then
      ./ebwt_search -n 1 -tqr ${NAME} ${READS} ${NAME}.ebwt.n1.${EXTRA_EXT}hits
 else
 	echo "${NAME}.ebwt.n1.hits already exists; skipping Maq-mode -n 1 run"
+fi
+
+# Run ebwt_search in end-to-end 1-mismatch mode to produce hits
+if [ ! -f ${NAME}.ebwt.1.${EXTRA_EXT}hits ] ; then
+   echo > ${NAME}.ebwt.1.${EXTRA_EXT}top
+   sh wrap.sh ${NAME}.ebwt.1${EXTRA_EXT} \
+     ./ebwt_search -1 -t ${NAME} ${READS} ${NAME}.ebwt.1.${EXTRA_EXT}hits
+else
+	echo "${NAME}.ebwt.1.hits already exists; skipping end-to-end 1 mismatch"
+fi
+
+# Run ebwt_search in end-to-end 2-mismatch mode to produce hits
+if [ ! -f ${NAME}.ebwt.2.${EXTRA_EXT}hits ] ; then
+   echo > ${NAME}.ebwt.2.${EXTRA_EXT}top
+   sh wrap.sh ${NAME}.ebwt.2${EXTRA_EXT} \
+     ./ebwt_search -2 -t ${NAME} ${READS} ${NAME}.ebwt.2.${EXTRA_EXT}hits
+else
+	echo "${NAME}.ebwt.2.hits already exists; skipping end-to-end 2 mismatch"
+fi
+
+# Possibly switch to filtered read set
+if [ "$USE_FILTERED_READS" = "1" ] ; then
+	USE_FILTERED_READS=0
+	goto begin
 fi
