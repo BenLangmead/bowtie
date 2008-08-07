87670dc8b118364bddc23e68d2a3c4d0937525a8
langmead <langmead>
2008-08-07 18:00:11 +0000

    *** empty log message ***

--- tests/paper_eval/kg_competition/analyze_maps.sh
+++ tests/paper_eval/kg_competition/analyze_maps.sh
@@ -3,125 +3,64 @@
 dir=`pwd`
 NAME=`basename $dir | sed 's/_.*//'`
 echo Using NAME: ${NAME}
-export TOT_READS=8956597
+export TOT_READS=8839010
+export TOT_FILT_READS=8400865
 
-if [ -f ${NAME}.ebwt.n1.hits ] ; then
-	if [ ! -f ${NAME}.ebwt.n1.reads.mapped ] ; then
-		awk '{print $1}' ${NAME}.ebwt.n1.hits > ${NAME}.ebwt.n1.reads.mapped
+do_one()
+{
+	NAME=$1
+	MAP_BASE=$2
+	MAP_EXT=$3
+	NREADS=$4
+	ISMAQ=$5
+	MAP="${MAP_BASE}.${MAP_EXT}"
+	RMAP="${MAP_BASE}.reads.mapped"
+	RMAPU="${MAP_BASE}.reads.mapped.uniq"
+	if [ -f ${MAP} ] ; then
+		if [ ! -f ${MAP_BASE}.reads.mapped ] ; then
+			if [ "$IS_MAQ" = "1" ] ; then
+				awk '{print $1}' ${MAP} > ${RMAP}
+			else
+				maq mapview ${MAP} | awk '{print $1}' > ${RMAP}
+			fi
+		fi
+		if [ ! -f ${RMAPU} ] ; then
+			sort -u ${RMAP} > ${RMAPU}
+		fi
+		num=`wc -l ${RMAP} | cut -d" " -f1`
+		numuniq=`wc -l ${RMAPU} | cut -d" " -f1`
+		if [ $num -ne $numuniq ] ; then
+			echo "${NAME}: Num hits: $num, num unique hits: $numuniq!"
+			echo "  Will use $numuniq for % reads mapped calculation"
+		fi
+		echo -n "${NAME}: % reads mapped: "
+		perl -e "print $numuniq * 100.0 / ${NREADS}"
+		echo
+	else
+		echo "Didn't find ${MAP}"
 	fi
-	if [ ! -f ${NAME}.ebwt.n1.reads.mapped.uniq ] ; then
-		sort -u ${NAME}.ebwt.n1.reads.mapped > ${NAME}.ebwt.n1.reads.mapped.uniq
-	fi
-	num=`wc -l ${NAME}.ebwt.n1.reads.mapped | cut -d" " -f1`
-	numuniq=`wc -l ${NAME}.ebwt.n1.reads.mapped.uniq | cut -d" " -f1`
-	if [ $num -ne $numuniq ] ; then
-		echo "Bowtie -n 1: Num hits: $num, num unique hits: $numuniq!"
-		echo "  Will use $numuniq for % reads mapped calculation"
-	fi
-	echo -n "Bowtie -n 1: % reads mapped: "
-	perl -e "print $numuniq * 100.0 / $TOT_READS"
-	echo
-else
-	echo "Didn't find ${NAME}.ebwt.n1.hits"
+}
+
+# Bowtie
+if [ 0 -gt 1 ] ; then
+	do_one "Bowtie -n 1" "${NAME}.ebwt.n1" "hits" "${TOT_READS}" "0"
 fi
+do_one "Bowtie" "${NAME}.ebwt" "hits" "${TOT_READS}" "0"
+do_one "Bowtie" "${NAME}.filt.ebwt" "hits" "${TOT_FILT_READS}" "0"
+do_one "Bowtie -v 2" "${NAME}.ebwt.2" "hits" "${TOT_READS}" "0"
 
-if [ -f ${NAME}.ebwt.hits ] ; then
-	if [ ! -f ${NAME}.ebwt.reads.mapped ] ; then
-		awk '{print $1}' ${NAME}.ebwt.hits > ${NAME}.ebwt.reads.mapped
-	fi
-	if [ ! -f ${NAME}.ebwt.reads.mapped.uniq ] ; then
-		sort -u ${NAME}.ebwt.reads.mapped > ${NAME}.ebwt.reads.mapped.uniq
-	fi
-	num=`wc -l ${NAME}.ebwt.reads.mapped | cut -d" " -f1`
-	numuniq=`wc -l ${NAME}.ebwt.reads.mapped.uniq | cut -d" " -f1`
-	if [ $num -ne $numuniq ] ; then
-		echo "Bowtie: Num hits: $num, num unique hits: $numuniq!"
-		echo "  Will use $numuniq for % reads mapped calculation"
-	fi
-	echo -n "Bowtie: % reads mapped: "
-	perl -e "print $numuniq * 100.0 / $TOT_READS"
-	echo
-else
-	echo "Didn't find ${NAME}.ebwt.hits"
+# Maq
+if [ 0 -gt 1 ] ; then
+	do_one "Maq -n 1" "${NAME}.maq.n1" "map" "${TOT_READS}" "1"
 fi
-
-if [ -f ${NAME}.maq.n1.map ] ; then
-	if [ ! -f ${NAME}.maq.n1.reads.mapped ] ; then
-		maq mapview ${NAME}.maq.n1.map | awk '{print $1}' > ${NAME}.maq.n1.reads.mapped
-	fi
-	if [ ! -f ${NAME}.maq.n1.reads.mapped.uniq ] ; then
-		sort -u ${NAME}.maq.n1.reads.mapped > ${NAME}.maq.n1.reads.mapped.uniq
-	fi
-	num=`wc -l ${NAME}.maq.n1.reads.mapped | cut -d" " -f1`
-	numuniq=`wc -l ${NAME}.maq.n1.reads.mapped.uniq | cut -d" " -f1`
-	if [ $num -ne $numuniq ] ; then
-		echo "Maq -n 1: Num hits: $num, num unique hits: $numuniq!"
-		echo "  Will use $numuniq for % reads mapped calculation"
-	fi
-	echo -n "Maq -n 1: % reads mapped: "
-	perl -e "print $numuniq * 100.0 / $TOT_READS"
-	echo
-else
-	echo "Didn't find ${NAME}.maq.n1.map"
+do_one "Maq" "${NAME}.maq" "map" "${TOT_READS}" "1"
+if [ 0 -gt 1 ] ; then
+	do_one "Maq -n 1" "${NAME}.maq.n1.filt" "map" "${TOT_FILT_READS}" "1"
 fi
+do_one "Maq" "${NAME}.maq.filt" "map" "${TOT_FILT_READS}" "1"
 
-if [ -f ${NAME}.maq.map ] ; then
-	if [ ! -f ${NAME}.maq.reads.mapped ] ; then
-		maq mapview ${NAME}.maq.map | awk '{print $1}' > ${NAME}.maq.reads.mapped
-	fi
-	if [ ! -f ${NAME}.maq.reads.mapped.uniq ] ; then
-		sort -u ${NAME}.maq.reads.mapped > ${NAME}.maq.reads.mapped.uniq
-	fi
-	num=`wc -l ${NAME}.maq.reads.mapped | cut -d" " -f1`
-	numuniq=`wc -l ${NAME}.maq.reads.mapped.uniq | cut -d" " -f1`
-	if [ $num -ne $numuniq ] ; then
-		echo "Maq: Num hits: $num, num unique hits: $numuniq!"
-		echo "  Will use $numuniq for % reads mapped calculation"
-	fi
-	echo -n "Maq: % reads mapped: "
-	perl -e "print $numuniq * 100.0 / $TOT_READS"
-	echo
-else
-	echo "Didn't find ${NAME}.maq.map"
+# Soap
+if [ 0 -gt 1 ] ; then
+	do_one "Soap -v 1" "${NAME}.soap.v1" "map" "${TOT_READS}" "0"
 fi
-
-if [ -f ${NAME}.soap.v1.map ] ; then
-	if [ ! -f ${NAME}.soap.v1.reads.mapped ] ; then
-		awk '{print $1}' ${NAME}.soap.v1.map > ${NAME}.soap.v1.reads.mapped
-	fi
-	if [ ! -f ${NAME}.soap.v1.reads.mapped.uniq ] ; then
-		sort -u ${NAME}.soap.v1.reads.mapped > ${NAME}.soap.v1.reads.mapped.uniq
-	fi
-	num=`wc -l ${NAME}.soap.v1.reads.mapped | cut -d" " -f1`
-	numuniq=`wc -l ${NAME}.soap.v1.reads.mapped.uniq | cut -d" " -f1`
-	if [ $num -ne $numuniq ] ; then
-		echo "Bowtie: Num hits: $num, num unique hits: $numuniq!"
-		echo "  Will use $numuniq for % reads mapped calculation"
-	fi
-	echo -n "Soap -v 1: % reads mapped: "
-	perl -e "print $numuniq * 100.0 / $TOT_READS"
-	echo
-else
-	echo "Didn't find ${NAME}.soap.v1.map"
-fi
-
-if [ -f ${NAME}.soap.v2.map ] ; then
-	if [ ! -f ${NAME}.soap.v2.reads.mapped ] ; then
-		awk '{print $1}' ${NAME}.soap.v2.map > ${NAME}.soap.v2.reads.mapped
-	fi
-	if [ ! -f ${NAME}.soap.v2.reads.mapped.uniq ] ; then
-		sort -u ${NAME}.soap.v2.reads.mapped > ${NAME}.soap.v2.reads.mapped.uniq
-	fi
-	num=`wc -l ${NAME}.soap.v2.reads.mapped | cut -d" " -f1`
-	numuniq=`wc -l ${NAME}.soap.v2.reads.mapped.uniq | cut -d" " -f1`
-	if [ $num -ne $numuniq ] ; then
-		echo "Bowtie: Num hits: $num, num unique hits: $numuniq!"
-		echo "  Will use $numuniq for % reads mapped calculation"
-	fi
-	echo -n "Soap -v 2: % reads mapped: "
-	perl -e "print $numuniq * 100.0 / $TOT_READS"
-	echo
-else
-	echo "Didn't find ${NAME}.soap.v2.map"
-fi
-
+do_one "Soap" "${NAME}.soap.v2" "map" "${TOT_READS}" "0"
