92e7fe4f10af678e9849651a7ca11b8f25578e7e
langmead <langmead>
2008-09-13 16:03:32 +0000

    *** empty log message ***

--- scripts/random_bowtie_tests.sh
+++ scripts/random_bowtie_tests.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
 echo "Short test emphasizing searching..."
-perl scripts/random_bowtie_tests.pl.pl 77 1000 200 300 200 8 30
+perl scripts/random_bowtie_tests.pl 77 1000 200 300 200 8 30
 echo "Short test emphasizing searching with short patterns..."
-perl scripts/random_bowtie_tests.pl.pl 77 1000 200 300 200 6 6
+perl scripts/random_bowtie_tests.pl 77 1000 200 300 200 6 6
 echo "Short test emphasizing searching with long patterns..."
-perl scripts/random_bowtie_tests.pl.pl 77 1000 200 300 200 30 20
+perl scripts/random_bowtie_tests.pl 77 1000 200 300 200 30 20
 echo "Short test emphasizing building..."
-perl scripts/random_bowtie_tests.pl.pl 77 1200 10 300 200 8 30
+perl scripts/random_bowtie_tests.pl 77 1200 10 300 200 8 30
 
 echo "Long test emphasizing searching..."
-perl scripts/random_bowtie_tests.pl.pl 77 5000 200 300 200 8 30
+perl scripts/random_bowtie_tests.pl 77 5000 200 300 200 8 30
 echo "Long test emphasizing building..."
-perl scripts/random_bowtie_tests.pl.pl 77 6000 10 300 200 8 30
+perl scripts/random_bowtie_tests.pl 77 6000 10 300 200 8 30
