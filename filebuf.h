3b0ec308ad2f7ce541cd1a0b0a816a358675ef97
langmead <langmead>
2008-11-17 06:11:05 +0000

    *** empty log message ***

--- filebuf.h
+++ filebuf.h
@@ -1,6 +1,7 @@
 #ifndef FILEBUF_H_
 #define FILEBUF_H_
 
+#include <iostream>
 #include <stdio.h>
 #include <stdint.h>
 
@@ -13,36 +14,51 @@
  */
 class FileBuf {
 public:
-	FileBuf(FILE *__in) : _in(__in), _cur(BUF_SZ), _buf_sz(BUF_SZ), _done(false) {
+	FileBuf(FILE *__in) : _in(__in), _ins(NULL), _inspos(0), _cur(BUF_SZ), _buf_sz(BUF_SZ), _done(false) {
 		assert(_in != NULL);
 	}
 
+	FileBuf(istream *__ins) : _in(NULL), _ins(__ins), _inspos(_ins.tellg()), _cur(BUF_SZ), _buf_sz(BUF_SZ), _done(false) {
+		assert(_ins != NULL);
+	}
+
 	int get() {
-		assert(_in != NULL);
+		assert(_in != NULL || _ins != NULL);
 		int c = peek();
 		if(c != -1) _cur++;
 		return c;
 	}
-	
+
 	bool eof() {
 		return (_cur == _buf_sz) && _done;
 	}
-	
+
 	void reset() {
-		rewind(_in);
+		if(_ins != NULL) {
+			_ins.seekg(_inspos);
+			_ins.clear();
+		} else {
+			rewind(_in);
+		}
 		_cur = BUF_SZ;
 		_buf_sz = BUF_SZ;
 		_done = false;
 	}
 
 	int peek() {
-		assert(_in != NULL);
+		assert(_in != NULL || _ins != NULL);
 		assert_leq(_cur, _buf_sz);
 		if(_cur == _buf_sz) {
 			if(_done) { return -1; }
 			else {
 				// Get the next chunk
-				_buf_sz = fread(_buf, 1, BUF_SZ, _in);
+				if(_ins != NULL) {
+					_ins.read(_buf, BUF_SZ);
+					_buf_sz = _ins.gcount();
+				} else {
+					assert(_in != NULL);
+					_buf_sz = fread(_buf, 1, BUF_SZ, _in);
+				}
 				_cur = 0;
 				if(_buf_sz == 0) {
 					_done = true;
@@ -79,11 +95,13 @@ public:
 	}
 private:
 	static const size_t BUF_SZ = 256 * 1024;
-	FILE   *_in;
-	size_t  _cur;
-	size_t  _buf_sz;
-	bool    _done;
-	char    _buf[BUF_SZ]; // (large) input buffer
+	FILE    *_in;
+	istream *_ins;
+	streampos _inspos;
+	size_t   _cur;
+	size_t   _buf_sz;
+	bool     _done;
+	char     _buf[BUF_SZ]; // (large) input buffer
 };
 
 #endif /*ndef FILEBUF_H_*/
