bce444d868112ab7c69cf8e9334930578f2be697
langmead <langmead>
2009-03-29 13:57:06 +0000

    *** empty log message ***

--- aligner_metrics.h
+++ aligner_metrics.h
@@ -82,6 +82,8 @@ public:
 		hiEntReads_(0),
 		alignedReads_(0),
 		unalignedReads_(0),
+		threeOrMoreNReads_(0),
+		lessThanThreeNRreads_(0),
 		bwtOpsPerRead_(),
 		backtracksPerRead_(),
 		bwtOpsPerHomoRead_(),
@@ -93,7 +95,15 @@ public:
 		bwtOpsPerAlignedRead_(),
 		backtracksPerAlignedRead_(),
 		bwtOpsPerUnalignedRead_(),
-		backtracksPerUnalignedRead_()
+		backtracksPerUnalignedRead_(),
+		bwtOpsPer0nRead_(),
+		backtracksPer0nRead_(),
+		bwtOpsPer1nRead_(),
+		backtracksPer1nRead_(),
+		bwtOpsPer2nRead_(),
+		backtracksPer2nRead_(),
+		bwtOpsPer3orMoreNRead_(),
+		backtracksPer3orMoreNRead_()
 		{ }
 
 	void printSummary() {
@@ -101,15 +111,19 @@ public:
 			finishRead();
 		}
 		cout << "AlignerMetrics:" << endl;
-		cout << "  # Reads:         " << reads_ << endl;
-		float lopct = (reads_ > 0) ? ((float)lowEntReads_+(float)homoReads_/((float)lowEntReads_+hiEntReads_)) : (0.0);
+		cout << "  # Reads:             " << reads_ << endl;
+		float lopct = (reads_ > 0) ? (((float)lowEntReads_+(float)homoReads_)/((float)reads_)) : (0.0);
 		lopct *= 100.0;
-		cout << "  % low-entropy:   " << (lopct) << endl;
-		float unpct = (reads_ > 0) ? ((float)unalignedReads_/((float)unalignedReads_+alignedReads_)) : (0.0);
+		cout << "  % low-entropy:       " << (lopct) << endl;
+		float unpct = (reads_ > 0) ? ((float)unalignedReads_/(float)(reads_)) : (0.0);
 		unpct *= 100.0;
-		cout << "  % unaligned:     " << (unpct) << endl;
+		cout << "  % unaligned:         " << (unpct) << endl;
+		float npct = (reads_ > 0) ? ((float)threeOrMoreNReads_/(float)(reads_)) : (0.0);
+		npct *= 100.0;
+		cout << "  % with 3 or more Ns: " << (npct) << endl;
 		cout << "  BWT ops:    avg: " << bwtOpsPerRead_.mean() << ", stddev: " << bwtOpsPerRead_.stddev() << endl;
 		cout << "  Backtracks: avg: " << backtracksPerRead_.mean() << ", stddev: " << backtracksPerRead_.stddev() << endl;
+		cout << endl;
 		cout << "  Homo-poly:" << endl;
 		cout << "    BWT ops:    avg: " << bwtOpsPerHomoRead_.mean() << ", stddev: " << bwtOpsPerLoEntRead_.stddev() << endl;
 		cout << "    Backtracks: avg: " << backtracksPerHomoRead_.mean() << ", stddev: " << backtracksPerLoEntRead_.stddev() << endl;
@@ -119,12 +133,26 @@ public:
 		cout << "  High-entropy:" << endl;
 		cout << "    BWT ops:    avg: " << bwtOpsPerHiEntRead_.mean() << ", stddev: " << bwtOpsPerHiEntRead_.stddev() << endl;
 		cout << "    Backtracks: avg: " << backtracksPerHiEntRead_.mean() << ", stddev: " << backtracksPerHiEntRead_.stddev() << endl;
+		cout << endl;
 		cout << "  Unaligned:" << endl;
 		cout << "    BWT ops:    avg: " << bwtOpsPerUnalignedRead_.mean() << ", stddev: " << bwtOpsPerUnalignedRead_.stddev() << endl;
 		cout << "    Backtracks: avg: " << backtracksPerUnalignedRead_.mean() << ", stddev: " << backtracksPerUnalignedRead_.stddev() << endl;
 		cout << "  Aligned:" << endl;
 		cout << "    BWT ops:    avg: " << bwtOpsPerAlignedRead_.mean() << ", stddev: " << bwtOpsPerAlignedRead_.stddev() << endl;
 		cout << "    Backtracks: avg: " << backtracksPerAlignedRead_.mean() << ", stddev: " << backtracksPerAlignedRead_.stddev() << endl;
+		cout << endl;
+		cout << "  0 Ns:" << endl;
+		cout << "    BWT ops:    avg: " << bwtOpsPer0nRead_.mean() << ", stddev: " << bwtOpsPerUnalignedRead_.stddev() << endl;
+		cout << "    Backtracks: avg: " << backtracksPer0nRead_.mean() << ", stddev: " << backtracksPerUnalignedRead_.stddev() << endl;
+		cout << "  1 N:" << endl;
+		cout << "    BWT ops:    avg: " << bwtOpsPer1nRead_.mean() << ", stddev: " << bwtOpsPerAlignedRead_.stddev() << endl;
+		cout << "    Backtracks: avg: " << backtracksPer1nRead_.mean() << ", stddev: " << backtracksPerAlignedRead_.stddev() << endl;
+		cout << "  2 Ns:" << endl;
+		cout << "    BWT ops:    avg: " << bwtOpsPer2nRead_.mean() << ", stddev: " << bwtOpsPerAlignedRead_.stddev() << endl;
+		cout << "    Backtracks: avg: " << backtracksPer2nRead_.mean() << ", stddev: " << backtracksPerAlignedRead_.stddev() << endl;
+		cout << "  >2 Ns:" << endl;
+		cout << "    BWT ops:    avg: " << bwtOpsPer3orMoreNRead_.mean() << ", stddev: " << bwtOpsPerAlignedRead_.stddev() << endl;
+		cout << "    Backtracks: avg: " << backtracksPer3orMoreNRead_.mean() << ", stddev: " << backtracksPerAlignedRead_.stddev() << endl;
 	}
 
 	/**
@@ -181,6 +209,23 @@ public:
 			bwtOpsPerUnalignedRead_.push((float)curBwtOps_);
 			backtracksPerUnalignedRead_.push((float)curBacktracks_);
 		}
+		if(curNumNs_ == 0) {
+			lessThanThreeNRreads_++;
+			bwtOpsPer0nRead_.push((float)curBwtOps_);
+			backtracksPer0nRead_.push((float)curBacktracks_);
+		} else if(curNumNs_ == 1) {
+			lessThanThreeNRreads_++;
+			bwtOpsPer1nRead_.push((float)curBwtOps_);
+			backtracksPer1nRead_.push((float)curBacktracks_);
+		} else if(curNumNs_ == 2) {
+			lessThanThreeNRreads_++;
+			bwtOpsPer2nRead_.push((float)curBwtOps_);
+			backtracksPer2nRead_.push((float)curBacktracks_);
+		} else {
+			threeOrMoreNReads_++;
+			bwtOpsPer3orMoreNRead_.push((float)curBwtOps_);
+			backtracksPer3orMoreNRead_.push((float)curBacktracks_);
+		}
 	}
 
 	// Running-total of the number of backtracks and BWT ops for the
@@ -198,6 +243,8 @@ protected:
 	bool curIsHomoPoly_;
 	// true iff the current read has had one or more ranges reported
 	bool curHadRanges_;
+	// number of Ns in current read
+	int curNumNs_;
 
 	// # reads
 	uint32_t reads_;
@@ -211,6 +258,10 @@ protected:
 	uint32_t alignedReads_;
 	// # reads without alignments
 	uint32_t unalignedReads_;
+	// # reads with 3 or more Ns
+	uint32_t threeOrMoreNReads_;
+	// # reads with < 3 Ns
+	uint32_t lessThanThreeNRreads_;
 
 	// Distribution of BWT operations per read
 	RunningStat bwtOpsPerRead_;
@@ -237,6 +288,23 @@ protected:
 	// Distribution of BWT operations per read that didn't align
 	RunningStat bwtOpsPerUnalignedRead_;
 	RunningStat backtracksPerUnalignedRead_;
+
+	// Distribution of BWT operations/backtracks per read with no Ns
+	RunningStat bwtOpsPer0nRead_;
+	RunningStat backtracksPer0nRead_;
+
+	// Distribution of BWT operations/backtracks per read with one N
+	RunningStat bwtOpsPer1nRead_;
+	RunningStat backtracksPer1nRead_;
+
+	// Distribution of BWT operations/backtracks per read with two Ns
+	RunningStat bwtOpsPer2nRead_;
+	RunningStat backtracksPer2nRead_;
+
+	// Distribution of BWT operations/backtracks per read with three or
+	// more Ns
+	RunningStat bwtOpsPer3orMoreNRead_;
+	RunningStat backtracksPer3orMoreNRead_;
 };
 
 #endif /* ALIGNER_METRICS_H_ */
