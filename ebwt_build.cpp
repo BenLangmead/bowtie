6d7b785c7449d7be90be21735d1ab12b394027ee
langmead <langmead>
2009-05-05 19:49:51 +0000

    *** empty log message ***

--- ebwt_build.cpp
+++ ebwt_build.cpp
@@ -656,7 +656,7 @@ int main(int argc, char **argv) {
 		     << ", " << sizeof(long) << ", " << sizeof(long long)
 		     << ", " << sizeof(void *) << ", " << sizeof(size_t)
 		     << ", " << sizeof(off_t) << "}" << endl;
-		cout << "Source hash: " << (int64_t)(EBWT_BUILD_HASH ll) << endl;
+		cout << "Source hash: " << (int64_t)(EBWT_BUILD_HASH) << endl;
 		return 0;
 	}
 
