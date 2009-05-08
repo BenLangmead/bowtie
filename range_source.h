27a2cb7e7168556923b96898604f45afc4134660
langmead <langmead>
2009-05-08 21:59:13 +0000

    *** empty log message ***

--- range_source.h
+++ range_source.h
@@ -939,7 +939,7 @@ public:
 		branchQ_.pop(); // remove it
 		if(verbose_) {
 			stringstream ss;
-			ss << patid_ << ": Popping " << b->id_ << ", " << b->cost_;
+			ss << patid_ << ": Popping " << b->id_ << ", " << b << ", " << b->cost_ << ", " << b->exhausted_ << ", " << b->curtailed_;
 			glog.msg(ss.str());
 		}
 		sz_--;
@@ -955,7 +955,7 @@ public:
 #endif
 		if(verbose_) {
 			stringstream ss;
-			ss << patid_ << ": Pushing " << b->id_ << ", " << b->cost_;
+			ss << patid_ << ": Pushing " << b->id_ << ", " << b << ", " << b->cost_ << ", " << b->exhausted_ << ", " << b->curtailed_;
 			glog.msg(ss.str());
 		}
 		branchQ_.push(b);
