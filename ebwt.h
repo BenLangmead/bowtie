c93be6d7af13e08e942543e7447a7a74cd2bf26b
langmead <langmead>
2009-06-30 20:08:10 +0000

    *** empty log message ***

--- ebwt.h
+++ ebwt.h
@@ -265,14 +265,14 @@ public:
 /**
  * Calculate size of file with given name.
  */
-static inline int fileSize(const char* name) {
+static inline int64_t fileSize(const char* name) {
 	std::ifstream f;
 	f.open(name, std::ios_base::binary | std::ios_base::in);
 	if (!f.good() || f.eof() || !f.is_open()) { return 0; }
 	f.seekg(0, std::ios_base::beg);
 	std::ifstream::pos_type begin_pos = f.tellg();
 	f.seekg(0, std::ios_base::end);
-	return static_cast<int>(f.tellg() - begin_pos);
+	return static_cast<int64_t>(f.tellg() - begin_pos);
 }
 
 // Forward declarations for Ebwt class
