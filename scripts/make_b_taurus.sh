c49baba4b1153887676433821685c737ba521d07
langmead <langmead>
2009-08-21 18:03:06 +0000

    *** empty log message ***

--- scripts/make_b_taurus.sh
+++ scripts/make_b_taurus.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 
 #
-# Builds an index from UMD Freeze 2.0 of the Bos Taurus (cow) genome.
+# Builds an index from UMD Freeze 3.0 of the Bos Taurus (cow) genome.
 #
 
 BOWTIE_BUILD_EXE=./bowtie-build
 
 INPUTS="/fs/szasmg3/bos_taurus/UMD_Freeze2.0/Chr1.fa"
-for chr in 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 X U ; do
-	INPUTS="${INPUTS},/fs/szasmg3/bos_taurus/UMD_Freeze2.0/Chr${chr}.fa"
+for chr in 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 X U Y-contigs.SHOTGUN_ONLY ; do
+	gzip -dc /fs/szasmg3/bos_taurus/UMD_Freeze3.0/Chr${chr}.fa.gz > Chr${chr}.fa.gz
+	INPUTS="${INPUTS},Chr${chr}.fa"
 done
 
 echo Running ${BOWTIE_BUILD_EXE} ${INPUTS} b_taurus
