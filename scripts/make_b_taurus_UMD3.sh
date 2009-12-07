6d4f4442830f54a52d572c8010c023b3c84e7750
langmead <langmead>
2009-12-07 19:57:06 +0000

    *** empty log message ***

--- scripts/make_b_taurus_UMD3.sh
+++ scripts/make_b_taurus_UMD3.sh
@@ -4,22 +4,86 @@
 # Builds an index from UMD Freeze 3.0 of the Bos Taurus (cow) genome.
 #
 
-BOWTIE_BUILD_EXE=./bowtie-build
+BASE_CHRS="\
+Chr1 \
+Chr2 \
+Chr3 \
+Chr4 \
+Chr5 \
+Chr6 \
+Chr7 \
+Chr8 \
+Chr9 \
+Chr10 \
+Chr11 \
+Chr12 \
+Chr13 \
+Chr14 \
+Chr15 \
+Chr16 \
+Chr17 \
+Chr18 \
+Chr19 \
+Chr20 \
+Chr21 \
+Chr22 \
+Chr23 \
+Chr24 \
+Chr25 \
+Chr26 \
+Chr27 \
+Chr28 \
+Chr29 \
+ChrX \
+ChrU \
+ChrY-contigs \
+ChrY-contigs.SHOTGUN_ONLY"
 
-INPUTS="/fs/szasmg3/bos_taurus/UMD_Freeze2.0/Chr1.fa"
-for chr in 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 X U Y-contigs.SHOTGUN_ONLY ; do
-	gzip -dc /fs/szasmg3/bos_taurus/UMD_Freeze3.0/Chr${chr}.fa.gz > Chr${chr}.fa.gz
-	INPUTS="${INPUTS},Chr${chr}.fa"
+CHRS_TO_INDEX=$BASE_CHRS
+
+get() {
+	file=$1
+	if ! wget --version >/dev/null 2>/dev/null ; then
+		if ! curl --version >/dev/null 2>/dev/null ; then
+			echo "Please install wget or curl somewhere in your PATH"
+			exit 1
+		fi
+		curl -o `basename $1` $1
+		return $?
+	else
+		wget $1
+		return $?
+	fi
+}
+
+BOWTIE_BUILD_EXE=./bowtie-build
+if [ ! -x "$BOWTIE_BUILD_EXE" ] ; then
+	if ! which bowtie-build ; then
+		echo "Could not find bowtie-build in current directory or in PATH"
+		exit 1
+	else
+		BOWTIE_BUILD_EXE=`which bowtie-build`
+	fi
+fi
+
+FTP_BASE=ftp://ftp.cbcb.umd.edu/pub/data/Bos_taurus/Bos_taurus_UMD_3.0
+OUTPUT=b_taurus
+
+INPUTS=
+for c in $CHRS_TO_INDEX ; do
+	if [ ! -f ${c}.fa ] ; then
+		F=${c}.fa.gz
+		get ${FTP_BASE}/$F || (echo "Error getting $F" && exit 1)
+		gunzip $F || (echo "Error unzipping $F" && exit 1)
+	fi
+	[ -n "$INPUTS" ] && INPUTS=$INPUTS,${c}.fa
+	[ -z "$INPUTS" ] && INPUTS=${c}.fa
 done
 
-echo Running ${BOWTIE_BUILD_EXE} ${INPUTS} b_taurus
-${BOWTIE_BUILD_EXE} ${INPUTS} b_taurus
-
-if [ "$?" = "0" ] ; then
-	echo "b_taurus index built:"
-	echo "   b_taurus.1.ebwt b_taurus.2.ebwt"
-	echo "   b_taurus.3.ebwt b_taurus.4.ebwt"
-	echo "   b_taurus.rev.1.ebwt b_taurus.rev.2.ebwt"
+CMD="$BOWTIE_BUILD_EXE $* $INPUTS $OUTPUT"
+echo $CMD
+if $CMD ; then
+	echo "$OUTPUT index built; you may remove fasta files"
 else
 	echo "Index building failed; see error message"
 fi
