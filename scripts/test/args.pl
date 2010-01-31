bf22cd79e001a0b6e131e7e0f3ee60ec6af38032
langmead <langmead>
2010-01-31 21:45:18 +0000

    *** empty log message ***

--- scripts/test/args.pl
+++ scripts/test/args.pl
@@ -7,13 +7,20 @@
 # and good ones are accepted.
 #
 
-my $debug = "-debug";
-my $bowtie = "./bowtie$debug";
+my $bowtie = "./bowtie";
+my $bowtie_d = "./bowtie-debug";
 if(system("$bowtie --version") != 0) {
-	$bowtie = `which bowtie$debug`;
+	$bowtie = `which bowtie`;
 	chomp($bowtie);
 	if(system("$bowtie --version") != 0) {
 		die "Could not find bowtie in current directory or in PATH\n";
+	}
+}
+if(system("$bowtie_d --version") != 0) {
+	$bowtie_d = `which bowtie-debug`;
+	chomp($bowtie_d);
+	if(system("$bowtie_d --version") != 0) {
+		die "Could not find bowtie-debug in current directory or in PATH\n";
 	}
 }
 
@@ -29,7 +36,9 @@ my @bad = (
 	"-5 -1",
 	"-e -1",
 	"-l 4",
-	"-l 0"
+	"-l 0",
+	"--gbar 0",
+	"--gbar -1"
 );
 
 my @good = (
@@ -41,14 +50,18 @@ my @good = (
 	"-v 1",
 	"-v 2",
 	"-v 3",
-	"-v 3 -n 3"
+	"-v 3 -n 3",
+	"--gbar 1",
+	"--gbar 99"
 );
 
 for my $a (@bad) {
-	system("$bowtie $a e_coli reads/e_coli_1000.fq /dev/null") != 0 || die;
+	system("$bowtie $a e_coli reads/e_coli_1000.fq /dev/null") != 0 || die "bowtie should have rejected: \"$a\"\n";
+	system("$bowtie_d $a e_coli reads/e_coli_1000.fq /dev/null") != 0 || die "bowtie-debug should have rejected: \"$a\"\n";
 	print "PASSED: bad args \"$a\"\n";
 }
 for my $a (@good) {
-	system("$bowtie $a e_coli reads/e_coli_1000.fq /dev/null") == 0 || die;
+	system("$bowtie $a e_coli reads/e_coli_1000.fq /dev/null") == 0 || die "bowtie should have accepted: \"$a\"\n";
+	system("$bowtie_d $a e_coli reads/e_coli_1000.fq /dev/null") == 0 || die "bowtie-debug should have accepted: \"$a\"\n";
 	print "PASSED: good args \"$a\"\n";
 }
