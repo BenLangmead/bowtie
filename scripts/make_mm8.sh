e5a7b5150a7c44cd1b386f6aebb380998e12e2c2
langmead <langmead>
2010-07-29 17:50:18 +0000

    *** empty log message ***

---
+++ scripts/make_mm8.sh
@@ -0,0 +1,103 @@
+#!/bin/sh
+
+#
+# Downloads sequence for the mm8 version of M. musculus (mouse) from
+# UCSC.
+#
+# Note that UCSC's mm8 build has two categories of compressed fasta
+# files:
+#
+# 1. The base files, named chr??.fa.gz
+# 2. The unplaced-sequence files, named chr??_random.fa.gz
+#
+# By default, this script builds and index for just the base files,
+# since alignments to those sequences are the most useful.  To change
+# which categories are built by this script, edit the CHRS_TO_INDEX
+# variable below.
+#
+
+BASE_CHRS="\
+chr1 \
+chr2 \
+chr3 \
+chr4 \
+chr5 \
+chr6 \
+chr7 \
+chr8 \
+chr9 \
+chr10 \
+chr11 \
+chr12 \
+chr13 \
+chr14 \
+chr15 \
+chr16 \
+chr17 \
+chr18 \
+chr19 \
+chrX \
+chrY \
+chrM"
+
+RANDOM_CHRS="\
+1_random \
+5_random \
+7_random \
+8_random \
+9_random \
+10_random \
+13_random \
+15_random \
+17_random \
+X_random \
+Y_random \
+Un_random"
+
+CHRS_TO_INDEX=$BASE_CHRS
+
+UCSC_MM8_BASE=ftp://hgdownload.cse.ucsc.edu/goldenPath/mm8/chromosomes
+
+get() {
+	file=$1
+	if ! wget --version >/dev/null 2>/dev/null ; then
+		if ! curl --version >/dev/null 2>/dev/null ; then
+			echo "Please install wget or curl somewhere in your PATH"
+			exit 1
+		fi
+		curl -o `basename $1` $1
+		return $?
+	else
+		wget $1
+		return $?
+	fi
+}
+
+BOWTIE_BUILD_EXE=./bowtie-build
+if [ ! -x "$BOWTIE_BUILD_EXE" ] ; then
+	if ! which bowtie-build ; then
+		echo "Could not find bowtie-build in current directory or in PATH"
+		exit 1
+	else
+		BOWTIE_BUILD_EXE=`which bowtie-build`
+	fi
+fi
+
+INPUTS=
+for c in $CHRS_TO_INDEX ; do
+	if [ ! -f ${c}.fa ] ; then
+		F=${c}.fa.gz
+		get ${UCSC_MM8_BASE}/$F || (echo "Error getting $F" && exit 1)
+		gunzip $F || (echo "Error unzipping $F" && exit 1)
+	fi
+	[ -n "$INPUTS" ] && INPUTS=$INPUTS,${c}.fa
+	[ -z "$INPUTS" ] && INPUTS=${c}.fa
+done
+
+CMD="${BOWTIE_BUILD_EXE} $* ${INPUTS} mm8"
+echo Running $CMD
+if $CMD ; then
+	echo "mm8 index built; you may remove fasta files"
+else
+	echo "Index building failed; see error message"
+fi
