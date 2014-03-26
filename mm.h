f65b0e7b527d6dfb93f9e618e4e8d206b284bd90
David K Jackson <david.jackson@sanger.ac.uk>
2014-03-26 09:47:11 +0000

    Use fopen (not open) with mmap
    As with jkbonfield's suggested patch to bowtie2 (merged in bowtie2
    commit 8f065de5), use "FILE *fp" in MM code rather than "int fd" to
    avoid 1 byte reads (which are very slow on some versions of Lustre).

--- mm.h
+++ mm.h
@@ -10,20 +10,11 @@
  * and where there isn't POSIX I/O,
  */
 
-#ifdef BOWTIE_MM
-#define MM_FILE_CLOSE(x) if(x > 3) { close(x); }
-#define MM_READ_RET ssize_t
-#define MM_READ read
-#define MM_SEEK lseek
-#define MM_FILE int
-#define MM_FILE_INIT -1
-#else
 #define MM_FILE_CLOSE(x) if(x != NULL) { fclose(x); }
 #define MM_READ_RET size_t
 #define MM_READ(file, dest, sz) fread(dest, 1, sz, file)
 #define MM_SEEK fseek
 #define MM_FILE FILE*
 #define MM_FILE_INIT NULL
-#endif
 
 #endif /* MM_H_ */
